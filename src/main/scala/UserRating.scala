95aea52725a2c674f0a05f2a448fd51b252f2e56
Ankush Khanna <a.khanna@rebuy.de>
2017-04-19 23:18:17 +0200

    Adding checkpoints.

--- src/main/scala/UserRating.scala
+++ src/main/scala/UserRating.scala
@@ -1,11 +1,12 @@
 import org.apache.spark.SparkConf
 import org.apache.spark.mllib.recommendation.{ALS, Rating}
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.storage.StorageLevel
 
 object UserRating {
   def main(args: Array[String]): Unit = {
     val conf = new SparkConf()
-      .set("spark.driver.maxResultSize", "3g")
+      .set("spark.driver.maxResultSize", "8g")
       .setMaster("local[4]")
       .set("spark.driver.cores", "5")
 
@@ -14,10 +15,11 @@ object UserRating {
       .config(conf)
       .getOrCreate()
 
-
+    sparkSession.sparkContext.setCheckpointDir("checkpoint/")
     val df = sparkSession.read
       .option("header", "true")
       .csv("data/ratings.csv")
+      .persist()
 
     print(df.count())
 
@@ -28,8 +30,14 @@ object UserRating {
     val array = data.randomSplit(weights = Array(6.0, 2.0, 2.0))
 
     val (train, cv, test) = (array(0), array(1), array(2))
-    for (r <- Array(4, 8, 12)) {
-      val model = ALS.train(train, r, 10, 0.1)
+    for (r <- Array(2, 4, 8, 12, 20, 40, 100)) {
+      val a = new ALS()
+        .setIterations(45)
+        .setRank(r)
+        .setLambda(0.1)
+        .setCheckpointInterval(2)
+
+      val model = a.run(train) //ALS.train(train, r, 15, 0.1)
 
       val usersProducts = cv.map { case Rating(user, product, rate) =>
         (user, product)
@@ -49,7 +57,7 @@ object UserRating {
         err * err
       }.mean()
 
-      println("Mean Squared Error = " + MSE)
+      println("Mean Squared Error = " + MSE + "    " + r)
     }
   }
 }
